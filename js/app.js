fd0c082ae935e3f5ab33b9fcda081e08d1c9932d
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-03-31 00:19:17 -0300

    📦 refactor: extract element creation with attribute to function

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
-const form = document.querySelector('.quiz-form')
-const popup = document.querySelector('.popup-wrapper')
-const popupContent = document.querySelector('.popup-content')
+const form = document.querySelector('.quiz-form');
+const popup = document.querySelector('.popup-wrapper');
+const popupContent = document.querySelector('.popup-content');
 
-const popupTitleElement = document.createElement('h2')
-const popupScoreElement = document.createElement('p')
+const createElementWithTestId = (element, testId) => {
+  const newElement = document.createElement(element);
+  newElement.setAttribute('data-testid', testId);
+  return newElement;
+};
 
-const correctAnswers = ['C', 'D', 'A', 'C', 'B']
+const popupTitleElement = createElementWithTestId('h2', 'popup-title');
+const popupScoreElement = createElementWithTestId('p', 'popup-score');
+
+const correctAnswers = ['C', 'D', 'A', 'C', 'B'];
 
 const getUserAnswers = () => {
   const userAnswers = [
@@ -14,75 +20,82 @@ const getUserAnswers = () => {
     form.inputQuestion3.value,
     form.inputQuestion4.value,
     form.inputQuestion5.value
-  ]
-  return userAnswers
-}
+  ];
+  return userAnswers;
+};
 
-const getScore = userAnswers => {
+const getScore = (userAnswers) => {
   const totalScore = userAnswers.reduce((score, userAnswer, index) => {
     if (userAnswer === correctAnswers[index]) {
-      score += 20
+      score += 20;
     }
-    return score
-  }, 0)
+    return score;
+  }, 0);
 
-  return totalScore
-}
+  return totalScore;
+};
 
-const animateScoreCounter = score => {
-  let counter = 0
-  
+const animateScoreCounter = (score) => {
+  let counter = 0;
+
   const timer = setInterval(() => {
     if (counter === score) {
-      clearInterval(timer)
+      clearInterval(timer);
     }
-    
-    popupScoreElement.textContent = `${counter}%`
-    counter++
-  }, 10)
+
+    popupScoreElement.textContent = `${counter}%`;
+    counter++;
+  }, 10);
 
   scrollTo({
     top: 0,
     left: 0,
     behavior: 'smooth'
-  })
-}
+  });
+};
 
 const showScorePopup = (scoreMessage, score) => {
-  popupTitleElement.textContent = scoreMessage
-  popupScoreElement.classList.add('popup-score')
-  
-  animateScoreCounter(score)
+  popupTitleElement.textContent = scoreMessage;
+  popupScoreElement.classList.add('popup-score');
 
-  popupContent.insertAdjacentElement('afterbegin', popupScoreElement)
-  popupContent.insertAdjacentElement('afterbegin', popupTitleElement)
-  popup.style.display = 'block'
-}
+  animateScoreCounter(score);
 
-const getScoreMessage = score => ({
-  0: 'Você errou todas =(',
-  100: 'Parabéns, você acertou todas!' 
-})[score] || `VOCÊ ACERTOU`
+  popupContent.insertAdjacentElement('afterbegin', popupScoreElement);
+  popupContent.insertAdjacentElement('afterbegin', popupTitleElement);
+  popup.style.display = 'block';
+};
 
-const showTotalScore = event => {
-  event.preventDefault()
+const getScoreMessage = (score) =>
+  ({
+    0: 'Você errou todas =(',
+    100: 'Parabéns, você acertou todas!'
+  }[score] || `VOCÊ ACERTOU`);
 
-  const userAnswers = getUserAnswers()
-  const score = getScore(userAnswers)
-  const scoreMessage = getScoreMessage(score)
+const showTotalScore = (event) => {
+  event.preventDefault();
 
-  showScorePopup(scoreMessage, score)
-}
+  const userAnswers = getUserAnswers();
+  const score = getScore(userAnswers);
+  const scoreMessage = getScoreMessage(score);
 
-const closePopup = event => {
-  const clickedElement = event.target
-  const classesThatClosePopup = ['popup-close', 'popup-button', 'popup-wrapper']
-  const clickedElementHasClass = classesThatClosePopup.includes(clickedElement.classList[0])
+  showScorePopup(scoreMessage, score);
+};
+
+const closePopup = (event) => {
+  const clickedElement = event.target;
+  const classesThatClosePopup = [
+    'popup-close',
+    'popup-button',
+    'popup-wrapper'
+  ];
+  const clickedElementHasClass = classesThatClosePopup.includes(
+    clickedElement.classList[0]
+  );
 
   if (clickedElementHasClass) {
-    popup.style.display = 'none'
+    popup.style.display = 'none';
   }
-}
+};
 
-form.addEventListener('submit', showTotalScore)
-popup.addEventListener('click', closePopup)
+form.addEventListener('submit', showTotalScore);
+popup.addEventListener('click', closePopup);
